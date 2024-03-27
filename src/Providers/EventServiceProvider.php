43b7bd3219f9164188f2b12aa359b1ecd6fa38f6
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-03-27 16:27:16 +0530

    Fix space

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -29,7 +29,7 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-          /**
+        /**
          * Styles loading.
          */
         Event::listen('bagisto.admin.layout.head', function ($viewRenderEventManager) {
